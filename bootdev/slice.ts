04b57aabd17c72a36546d6ac3495e8d16d630768
LeonelCedillo <leonel.cs.979@gmail.com>
2026-07-24 13:29:05 -0700

    complete slicing practice - ts

---
+++ bootdev/slice.ts
@@ -0,0 +1,10 @@
+// My Solution:
+
+function summarizeSignal(signal: string): string {
+  const first = signal.slice(0,4);
+  const middle = signal.slice(4,-2);
+  const tail = signal.slice(-2);
+  const full = signal.slice();
+  const beyond = signal.slice(30);
+  return `first: ${first} | middle: ${middle} | tail: ${tail} | full: ${full} | beyond: ${beyond}`;
+}
